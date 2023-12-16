2b90f498167866858b5b6324890a1b2ebdc4daf7
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-17 02:11:51 +0300

    Add through Class 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 class Employee:
 
+    num_of_emps = 0
     raise_amount = 1.04
 
     def __init__(self, first, last, pay):
@@ -10,6 +11,7 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + 'company.com'
 
+        Employee.num_of_emps += 1
 
     # @property
     def fullname(self):
@@ -22,16 +24,5 @@ class Employee:
 emp_1 = Employee("Corey", "Schafer", 50000)
 emp_2 = Employee("Jon", "Tracy", 60000)
 
-Employee.raise_amount = 1.10
-# emp_2.raise_amount = 1.10
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
-
-print(emp_2.pay)
-emp_2.apply_raise()
-print(emp_2.pay)
-
-# print(emp_1.__dict__)
-# print(Employee.__dict__)
+print(Employee.num_of_emps)
 
