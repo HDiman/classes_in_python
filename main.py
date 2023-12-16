b783b267142881f1729470473cf7d9e22edf03f0
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-16 23:19:37 +0300

    Create class with init 2

--- main.py
+++ main.py
@@ -5,23 +5,15 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
+        self.email = first + '.' + last + 'company.com'
 
 
 emp_1 = Employee("Corey", "Schafer", 50000)
 emp_2 = Employee("Jon", "Tracy", 60000)
 
-print(emp_1)
-print(emp_2)
-
-# emp_1.first = "Corey"
-# emp_1.last = "Schafer"
-# emp_1.email = 'Corey.Schafer@company.com'
-# emp_1.pay = 50000
-#
-# emp_2.first = "Jon"
-# emp_2.last = "Tracy"
-# emp_2.email = 'Jon.Tracy@company.com'
-# emp_2.pay = 60000
 
 print(emp_1.first)
 print(emp_2.first)
+
+print(emp_1.email)
+print(emp_2.email)
