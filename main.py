2f28a02f1d0c3ef8a3d9e2836e2778548f3242b0
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-16 23:16:57 +0300

    Create class with init

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+# Python Object-Oriented Programming
 
 class Employee:
-    pass
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+
+emp_1 = Employee("Corey", "Schafer", 50000)
+emp_2 = Employee("Jon", "Tracy", 60000)
+
+print(emp_1)
+print(emp_2)
+
+# emp_1.first = "Corey"
+# emp_1.last = "Schafer"
+# emp_1.email = 'Corey.Schafer@company.com'
+# emp_1.pay = 50000
+#
+# emp_2.first = "Jon"
+# emp_2.last = "Tracy"
+# emp_2.email = 'Jon.Tracy@company.com'
+# emp_2.pay = 60000
+
+print(emp_1.first)
+print(emp_2.first)
