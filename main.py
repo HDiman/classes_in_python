64e71ac98760b7bef70c8e0fb06cebdbfd10c074
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-17 00:39:21 +0300

    Add property

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + 'company.com'
 
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
@@ -15,5 +16,5 @@ emp_1 = Employee("Corey", "Schafer", 50000)
 emp_2 = Employee("Jon", "Tracy", 60000)
 
 
-print(emp_1.fullname())
-print(emp_2.fullname())
+print(emp_1.fullname)
+print(emp_2.fullname)
