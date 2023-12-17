cea8bab257dd90b7687b40e6fb016bda460a0381
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-17 12:00:31 +0300

    Set values through Class method

--- playground.py
+++ playground.py
@@ -1,9 +1,37 @@
+# ============= After 5 lesson =============
+
+
+
+
+
+# ============= After 4 lesson =============
+
+
+
+
+# ============= After 3 lesson =============
+# Regular, Class and Static methods
+# Regular takes Instance as first Argument
+# Class methods = @classmethod
+
+
+
+
+
+
+
+
+
+
+
+# ============= After 2 lessons =============
+
 # Python Object-Oriented Programming
 
 class Employee:
 
     num_of_emps = 0
-    raise_amt = 1.04
+    raise_amount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -18,19 +46,10 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
-
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amt = amount
+        self.pay = int(self.pay * self.raise_amount)
 
 
 emp_1 = Employee("Corey", "Schafer", 50000)
 emp_2 = Employee("Jon", "Tracy", 60000)
 
-Employee.set_raise_amt(1.05)
-
-print(Employee.raise_amt)
-print(emp_1.raise_amt)
-print(emp_2.raise_amt)
-
+print(Employee.num_of_emps)
