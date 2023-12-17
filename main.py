92b045655298a569b2bf3568dcce37ed4c827be6
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-17 14:46:20 +0300

    Special Methods like __add__, __len__, __repr__, __str__

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 class Employee:
 
-    num_of_emps = 0
     raise_amt = 1.04
 
     def __init__(self, first, last, pay):
@@ -17,24 +16,31 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amt = amount
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
 
 
-class Developer(Employee):
-    raise_amt = 1.10
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Jon', 'Tracy', 60000)
 
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)
-        self.prog_lang = prog_lang
+print(emp_1)
+print(repr(emp_1))
+print(str(emp_1))
 
+print(1 + 2)
+print(int.__add__(1, 2))
+print(int.__sub__(10, 5))
 
-dev_1 = Developer("Corey", "Schafer", 50000, 'Python')
-dev_2 = Developer("Jon", "Tracy", 60000, 'Java')
+print(Employee.__add__(emp_1, emp_2))
+print(emp_1 + emp_2)
 
-print(dev_1.email)
-print(dev_1.prog_lang)
-
-
-
+print(len(emp_1))
--- playground.py
+++ playground.py
@@ -1,18 +1,207 @@
+# ============= After 6 lesson =============
+
+
+# ---------------------------------------
+
+
+# ---------------------------------------
+
+
 # ============= After 5 lesson =============
 
+# special methods
+# return Notimplemented
+# Python Object-Oriented Programming
 
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def __repr__(self):
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Jon', 'Tracy', 60000)
+
+print(emp_1)
+print(repr(emp_1))
+print(str(emp_1))
+
+print(1 + 2)
+print(int.__add__(1, 2))
+print(int.__sub__(10, 5))
+
+print(Employee.__add__(emp_1, emp_2))
+print(emp_1 + emp_2)
+
+print(len(emp_1))
 
 
 
 # ============= After 4 lesson =============
+# print(isinstance(mgr_1, Manager))
+# print(issubclass(Developer, Employee))
+# Python Object-Oriented Programming
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    # raise_amt = 1.07
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Jon', 'Tracy', 60000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(mgr_1.email)
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
+
+print(isinstance(mgr_1, Manager))
+print(issubclass(Developer, Employee))
+
+
+# ---------------------------------------
+
 
 # super().__init__(first, last, pay)
+# Python Object-Oriented Programming
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
 
 
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
 
+class Manager(Employee):
+    # raise_amt = 1.07
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
 
 
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Jon', 'Tracy', 60000, 'Java')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+print(mgr_1.email)
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
 
 # ---------------------------------------
 
