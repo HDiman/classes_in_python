6b319dee4a4e951c836007552ea7010d45673e25
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-12-17 15:12:20 +0300

    Setters

--- main.py
+++ main.py
@@ -2,45 +2,42 @@
 
 class Employee:
 
-    raise_amt = 1.04
-
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@company.com'
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
-
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
-
-    def __add__(self, other):
-        return self.pay + other.pay
-
-    def __len__(self):
-        return len(self.fullname())
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
 
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Jon', 'Tracy', 60000)
 
-print(emp_1)
-print(repr(emp_1))
-print(str(emp_1))
+emp_1 = Employee('John', 'Smith')
 
-print(1 + 2)
-print(int.__add__(1, 2))
-print(int.__sub__(10, 5))
+emp_1.fullname = 'Corey Schafer'
 
-print(Employee.__add__(emp_1, emp_2))
-print(emp_1 + emp_2)
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
 
-print(len(emp_1))
+del emp_1.fullname
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
